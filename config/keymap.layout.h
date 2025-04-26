5eba4d248c3c638ca30d3f136ef05417107c902b
Kuhlklay <kuhlklay@gmail.com>
2025-04-26 22:28:38 +0200

    Update corne.combos.dtsi

---
+++ config/keymap.layout.h
@@ -0,0 +1,55 @@
+#pragma once
+
+/*                         KEY POSITIONS
+  ╭─────────────────────────╮         ╭─────────────────────────╮
+  │ --0 --1 --2 --3 --4 --5 │         │ --6 --7 --8 --9 -10 -11 │
+  │ -12 -13 -14 -15 -16 -17 │         │ -18 -19 -20 -21 -22 -23 │
+  │ -24 -25 -26 -27 -28 -29 ╰───╮ ╭───╯ -30 -31 -32 -33 -34 -35 │
+  ╰───────────────╮ -36 -37 -38 │ │ -39 -40 -41 ╭───────────────╯
+                  ╰─────────────╯ ╰─────────────╯ */
+
+#define ATL 0
+#define TL4 1
+#define TL3 2
+#define TL2 3
+#define TL1 4
+#define TL0 5
+#define TR0 6
+#define TR1 7
+#define TR2 8
+#define TR3 9
+#define TR4 10
+#define ATR 11
+
+#define AML 12
+#define ML4 13
+#define ML3 14
+#define ML2 15
+#define ML1 16
+#define ML0 17
+#define MR0 18
+#define MR1 19
+#define MR2 20
+#define MR3 21
+#define MR4 22
+#define AMR 23
+
+#define ABL 24
+#define BL4 25
+#define BL3 26
+#define BL2 27
+#define BL1 28
+#define BL0 29
+#define BR0 30
+#define BR1 31
+#define BR2 32
+#define BR3 33
+#define BR4 34
+#define ABR 35
+
+#define HL1 36
+#define HL0 37
+#define HLT 38
+#define HRT 39
+#define HR0 40
+#define HR1 41
